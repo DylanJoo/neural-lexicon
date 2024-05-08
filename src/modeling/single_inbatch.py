49115b8f2651a1d4157d72df4aedbce6785f26a5
DylanJoo <dylanjootw@gmail.com>
2024-05-08 08:36:52 +0200

    revise dist setup

--- src/modeling/single_inbatch.py
+++ src/modeling/single_inbatch.py
@@ -1,17 +1,22 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.distributed as dist
+
 import random
 from dataclasses import dataclass
 from typing import Optional, Tuple, Dict, List, Mapping
 from transformers.modeling_outputs import BaseModelOutput
-# from .load_utils import load_state_dict_modified
+from src.modeling import dist_utils 
 
 @dataclass
 class InBatchOutput(BaseModelOutput):
     loss: torch.FloatTensor = None
     acc: Optional[Tuple[torch.FloatTensor, ...]] = None
     logs: Optional[Dict[str, torch.FloatTensor]] = None
+    qemb: Optional[torch.FloatTensor] = None
+    cemb: Optional[torch.FloatTensor] = None
+    spemb: Optional[torch.FloatTensor] = None
 
 class InBatchInteraction(nn.Module):
 
@@ -35,14 +40,13 @@ class InBatchInteraction(nn.Module):
         self.label_smoothing = label_smoothing
         self.tokenizer = tokenizer
 
+        # distributed 
+        self.is_ddp = dist.is_initialized()
+
         # learning hyperparameter
         self.bidirectional = False
         self.tau = opt.temperature
         self.tau_span = opt.temperature_span
-
-        ## additional projection layer
-        # hidden_size = retriever.config.hidden_size
-        # self.non_linear = nn.Sequential(nn.Linear(2*hidden_size, hidden_size), nn.Tanh())
 
         ## negative miner
         self.miner = miner
@@ -58,25 +62,29 @@ class InBatchInteraction(nn.Module):
         """
         [todo] see if therea anything different between dynamic and static negative vectors 
         """
-        bsz = len(q_tokens)
-        labels = torch.arange(0, bsz, dtype=torch.long, device=q_tokens.device)
-
         loss = 0.0
         qemb = self.encoder(input_ids=q_tokens, attention_mask=q_mask)[0]
         cemb = self.encoder(input_ids=c_tokens, attention_mask=c_mask)[0]
 
+        if self.is_ddp:
+            gather_fn = dist_utils.gather
+            qemb = gather_fn(qemb)
+            cemb = gather_fn(cemb)
+            data_index = gather_fn(data_index).detach().cpu().numpy()
+
+        bsz = qemb.size(0) # query-centric batch 
+        labels = torch.arange(0, bsz, dtype=torch.long, device=q_tokens.device)
+
         ## [st-st]
-        ### [revise here] !!!!
         ### negative mining using precomputed embeddings
         if self.miner is not None:
             neg_inputs = self.miner.crop_depedent_from_docs(
-                    embeds_1=qemb.detach().clone().cpu(), 
-                    embeds_2=cemb.detach().clone().cpu(),
+                    embeds_1=qemb.clone().detach().cpu(), 
+                    embeds_2=cemb.clone().detach().cpu(),
                     indices=data_index,
                     n=1, k0=0, k=100, 
                     exclude_overlap=True,
-                    return_token_ids=True,
-                    start_from_hit=True,
+                    to_return='span',
                     debug=self.opt.my_debug
             )
             neg_vectors = self.encoder(
@@ -114,6 +122,7 @@ class InBatchInteraction(nn.Module):
 
         loss_sp = 0.0
         loss_distil = 0.0
+
         ## [st-sp]
         ### query-span & context-span contrastive 
         if span_tokens is not None and span_mask is not None:
@@ -122,6 +131,9 @@ class InBatchInteraction(nn.Module):
                     attention_mask=span_mask, 
                     pooling=self.opt.span_pooling
             )[0]
+            if self.is_ddp:
+                gather_fn = dist_utils.gather
+                spemb = gather_fn(spemb)
 
             if self.miner is not None: 
                 # [todo] for stsp objective, see what's the better doc mining strategy
@@ -130,8 +142,6 @@ class InBatchInteraction(nn.Module):
                             embeds=spemb.detach().clone().cpu(), 
                             indices=data_index,
                             n=1, k0=0, k=100,
-                            start_from_hit=True,
-                            return_token_ids=True
                     )
                     neg_vectors = self.encoder(
                             input_ids=neg_inputs[0].to(self.encoder.device),
@@ -146,13 +156,8 @@ class InBatchInteraction(nn.Module):
             if neg_vectors is not None:
                 scores_qsp = torch.einsum("id, jd->ij", 
                         qemb / self.tau, torch.cat([spemb, neg_vectors], dim=0))
-                # add bidrectional
-                # scores_spq = torch.einsum("id, jd->ij", 
-                #         spemb / self.tau, torch.cat([qemb, neg_vectors], dim=0))
                 scores_csp = torch.einsum("id, jd->ij", 
                         cemb / self.tau, torch.cat([spemb, neg_vectors], dim=0))
-                # scores_spc = torch.einsum("id, jd->ij", 
-                #         spemb / self.tau, torch.cat([cemb, neg_vectors], dim=0))
             else:
                 scores_qsp = torch.einsum("id, jd->ij", qemb / self.tau_span, spemb)
                 scores_csp = torch.einsum("id, jd->ij", cemb / self.tau_span, spemb)
@@ -174,7 +179,7 @@ class InBatchInteraction(nn.Module):
             logs.update(self.miner.additional_log)
 
         loss = loss * self.opt.alpha + loss_sp * self.opt.beta + loss_distil * self.opt.gamma
-        return InBatchOutput(loss=loss, acc=accuracy, logs=logs)
+        return InBatchOutput(loss=loss, acc=accuracy, logs=logs, qemb=qemb, cemb=cemb, spemb=spemb)
 
     def get_encoder(self):
         return self.encoder
@@ -182,58 +187,58 @@ class InBatchInteraction(nn.Module):
     # def forward_bidirectional(self, tokens, mask, **kwargs):
     # becoming the default setup as it seems to be better
 
-class InBatchInteractionWithSpan(InBatchInteraction):
-
-    def forward(self, q_tokens, q_mask, c_tokens, c_mask, span_tokens, span_mask, **kwargs):
-
-        bsz = len(q_tokens)
-        labels = torch.arange(0, bsz, dtype=torch.long, device=q_tokens.device)
-
-        # [objectives] 
-        CELoss = nn.CrossEntropyLoss()
-        KLLoss = nn.KLDivLoss(reduction='batchmean')
-        MSELoss = nn.MSELoss()
-
-        # add the dataclass for outputs
-        qemb, qsemb = self.encoder(input_ids=q_tokens, attention_mask=q_mask)
-        cemb, csemb = self.encoder(input_ids=c_tokens, attention_mask=c_mask)
-        semb, ssemb = self.encoder(input_ids=span_tokens, attention_mask=span_mask)
-
-        if self.norm_query:
-            qemb = F.normalize(qemb, p=2, dim=-1)
-            qsemb = F.normalize(qsemb, p=2, dim=-1)
-        if self.norm_doc:
-            cemb = F.normalize(cemb, p=2, dim=-1)
-            csemb = F.normalize(csemb, p=2, dim=-1)
-
-        logs = {}
-        # [sentence]
-        scores = torch.einsum("id, jd->ij", qemb / self.tau, cemb)
-        loss_0 = CELoss(scores, labels)
-        predicted_idx = torch.argmax(scores, dim=-1)
-        accuracy = 100 * (predicted_idx == labels).float().mean()
-        logs.update({'loss_sent': loss_0, 'acc_sent': accuracy})
-
-        # [span-sent interaction]
-        ## add loss of (q-span, doc) ## add loss of (query, d-span)
-        scores_spans = torch.einsum("id, jd->ij", qsemb / self.tau_span, cemb)
-
-        if self.opt.span_span_interaction:
-            scores_spans2 = torch.einsum("id, jd->ij", csemb / self.tau_span, qsemb)
-            loss_span = CELoss(scores_spans, labels) + CELoss(scores_spans2, labels) 
-            logs.update({'acc_span2': accuracy_span2})
-        else:
-            loss_span = CELoss(scores_spans, labels) 
-
-        predicted_idx = torch.argmax(scores_spans, dim=-1) # check only one as it's prbbly similar
-        accuracy_span = 100 * (predicted_idx == labels).float().mean()
-
-        logs.update({'loss_span': loss_span, 'acc_span': accuracy_span})
-        logs.update(self.encoder.additional_log)
-
-        loss = loss_0 * self.opt.alpha + loss_span * self.opt.beta
-        return InBatchOutput(
-	        loss=loss, 
-	        acc=accuracy,
-                logs=logs,
-	)
+# class InBatchInteractionWithSpan(InBatchInteraction):
+#
+#     def forward(self, q_tokens, q_mask, c_tokens, c_mask, span_tokens, span_mask, **kwargs):
+#
+#         bsz = len(q_tokens)
+#         labels = torch.arange(0, bsz, dtype=torch.long, device=q_tokens.device)
+#
+#         # [objectives] 
+#         CELoss = nn.CrossEntropyLoss()
+#         KLLoss = nn.KLDivLoss(reduction='batchmean')
+#         MSELoss = nn.MSELoss()
+#
+#         # add the dataclass for outputs
+#         qemb, qsemb = self.encoder(input_ids=q_tokens, attention_mask=q_mask)
+#         cemb, csemb = self.encoder(input_ids=c_tokens, attention_mask=c_mask)
+#         semb, ssemb = self.encoder(input_ids=span_tokens, attention_mask=span_mask)
+#
+#         if self.norm_query:
+#             qemb = F.normalize(qemb, p=2, dim=-1)
+#             qsemb = F.normalize(qsemb, p=2, dim=-1)
+#         if self.norm_doc:
+#             cemb = F.normalize(cemb, p=2, dim=-1)
+#             csemb = F.normalize(csemb, p=2, dim=-1)
+#
+#         logs = {}
+#         # [sentence]
+#         scores = torch.einsum("id, jd->ij", qemb / self.tau, cemb)
+#         loss_0 = CELoss(scores, labels)
+#         predicted_idx = torch.argmax(scores, dim=-1)
+#         accuracy = 100 * (predicted_idx == labels).float().mean()
+#         logs.update({'loss_sent': loss_0, 'acc_sent': accuracy})
+#
+#         # [span-sent interaction]
+#         ## add loss of (q-span, doc) ## add loss of (query, d-span)
+#         scores_spans = torch.einsum("id, jd->ij", qsemb / self.tau_span, cemb)
+#
+#         if self.opt.span_span_interaction:
+#             scores_spans2 = torch.einsum("id, jd->ij", csemb / self.tau_span, qsemb)
+#             loss_span = CELoss(scores_spans, labels) + CELoss(scores_spans2, labels) 
+#             logs.update({'acc_span2': accuracy_span2})
+#         else:
+#             loss_span = CELoss(scores_spans, labels) 
+#
+#         predicted_idx = torch.argmax(scores_spans, dim=-1) # check only one as it's prbbly similar
+#         accuracy_span = 100 * (predicted_idx == labels).float().mean()
+#
+#         logs.update({'loss_span': loss_span, 'acc_span': accuracy_span})
+#         logs.update(self.encoder.additional_log)
+#
+#         loss = loss_0 * self.opt.alpha + loss_span * self.opt.beta
+#         return InBatchOutput(
+# 	        loss=loss, 
+# 	        acc=accuracy,
+#                 logs=logs,
+# 	)
