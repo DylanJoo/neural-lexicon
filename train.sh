e62017527775b8f5a6ca0e41fe57444a1172db2c
DylanJoo <dylanjootw@gmail.com>
2024-05-08 08:55:15 +0200

    hide dev codes

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH --job-name=tas.w
+#SBATCH --job-name=SP+HN
 #SBATCH --partition gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:nvidia_titan_v:2
 #SBATCH --mem=15G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=02:00:00
+#SBATCH --time=05:00:00
 #SBATCH --output=%x.%j.out
 
 # Set-up the environment.
@@ -16,30 +16,32 @@ backbone=contriever
 ckpt=facebook/contriever
 
 # Start the experiment.
-for dataset in scifact scidocs;do
-for method in dev;do
+for dataset in scidocs scifact;do
+for dummy in 0.0;do
+    method=sp-en-hn-strong
     exp=${method}
 
     # Go
     torchrun --nproc_per_node 2 \
         train.py \
-        --model_name facebook/contriever \
-        --train_data_dir /home/dju/datasets/temp/${dataset} \
-        --loading_mode from_precomputed \
+        --model_name ${ckpt} \
+        --train_data_dir /home/dju/datasets/beir/${dataset}/dw-ind-cropping \
+        --loading_mode doc2spans_gte \
+        --select_span_mode weighted \
         --output_dir models/ckpt/${backbone}-${exp}/${dataset} \
         --per_device_train_batch_size 32 \
-        --temperature 0.1 --temperature_span 0.5 \
-        --select_span_mode weighted \
+        --temperature 0.1 --temperature_span 0.1 \
         --pooling mean --span_pooling mean \
-        --alpha 1.0 --beta 0.0 \
-        --do_tas_doc \
+        --alpha 1.0 --beta 1.0 --gamma 0.5 \
         --chunk_length 256 \
-        --save_strategy steps \
-        --max_steps 2000 \
-        --save_steps 1000 \
-        --save_total_limit 2 \
+        --save_strategy epoch \
+        --num_train_epochs 4 \
+        --save_total_limit 4 \
         --warmup_ratio 0.1 \
-        --fp16 --wandb_project tas_span \
-        --report_to wandb --run_name ${dataset}-${exp}.tas.weighted
+        --fp16 --wandb_project ssldr-exp1 \
+        --report_to wandb --run_name ${dataset}-${exp} \
+        --do_negative_sampling \
+        --mine_neg_using crops \
+        --prebuilt_index_dir /home/dju/indexes/temp/doc_emb_ctrv_${dataset}
 done
 done
